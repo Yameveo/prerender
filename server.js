25663e610a81a2443798234f89854ea369596274
Andrea De Pirro <andreadepirro@gmail.com>
2015-03-13 17:37:09 +0100

    Enable cookies

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT,
+    cookiesEnabled: true
 });
 
 
